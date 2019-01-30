b5ea288d6b7d1e6769a01f2a72c333f74af2f6c9
Jerome Hanciles <jhanciles@spartaglobal.com>
2019-01-30 08:23:21 +0000

    homework updated

--- js/calculator.js
+++ js/calculator.js
@@ -1,6 +1,38 @@
+var value1 = null;
+var choice = null;
+var value2 = null;
+var answer = null;
+var newclose = null;
 
-    // get the user inputs
-    var value1 = prompt('enter first value'); 
-    var value2 = prompt('enter the second value');
-    var choice = prompt('Choose your action (a)dd (s)ubtract (m)ultiply (d)ivide') || "a";
+do {
+  var value1 = parseFloat(prompt('enter first value'));
+  var choice = prompt('Choose your action (a)dd  (s)ubtract (m)ultiply (d)ivide') || "a";
+  var value2 = parseFloat(prompt('enter the second value'));
 
+  if (choice == "") {
+    alert("add will be selected as default if no operator is chosen");
+  }
+
+  switch (choice) {
+  case "a":
+  answer = value1 + value2
+  alert(answer);
+  break;
+  case "s":
+  answer = value1 - value2
+  alert(answer);
+  break;
+  case "m":
+  answer = value1 * value2
+  alert(answer);
+  break;
+  case "d":
+  answer = value1 / value2
+  alert(answer);
+  break;
+  default:
+    alert("Operation not valid")
+  }
+
+   newclose = prompt("Enter 'close' to exit")
+} while ( newclose != "close");
